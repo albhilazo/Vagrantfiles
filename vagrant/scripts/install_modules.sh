be99a5e8586852cd92120a3f71549b1d905c299b
Albert Hilazo <albhilazo@gmail.com>
2015-12-06 04:29:43 +0100

    Added example script to download Puppet modules

---
+++ vagrant/scripts/install_modules.sh
@@ -0,0 +1,8 @@
+# Script to download dependencies as Puppet modules
+
+# puppet module install <author>/<module> --modulepath $1 --version <version-number>
+
+# yum install -y git
+# git clone <repo-url> $1/mymodule
+
+exit 0
