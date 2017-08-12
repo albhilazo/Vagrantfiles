d9ea26c8fc08e98bb285c952ead7b94c0aba8f05
Albert Hilazo <albhilazo@gmail.com>
2017-08-12 15:27:10 +0200

    Remove Puppet-specific files and extract Vagrantfile configuration

--- vagrant/scripts/install_modules.sh
+++
@@ -1,8 +0,0 @@
-# Script to download dependencies as Puppet modules
-
-# puppet module install <author>/<module> --modulepath $1 --version <version-number>
-
-# yum install -y git
-# git clone <repo-url> $1/mymodule
-
-exit 0
